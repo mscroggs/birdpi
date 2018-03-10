2e3982650957fe90ec09990ae7d30352286701d0
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2018-03-10 22:02:38 +0000

    Initial commit

---
+++ www/index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>Birdpi</title>
+<link rel='stylesheet' type='text/css' src='sty.css'>
+</head>
+<body>
+<div class='header'>
+<?php
+
+echo "Hello there";
+
+?>
+
+</body>
+</html>
